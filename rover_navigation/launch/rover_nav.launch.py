a48deb93db98a2972f205c9293654d59ef04c5c8
radupotlog <potlogradu@gmail.com>
2026-09-12 12:40:12 +0300

    Remaping cmd_vel

--- rover_navigation/launch/rover_nav.launch.py
+++ rover_navigation/launch/rover_nav.launch.py
@@ -159,6 +159,7 @@ def generate_launch_description():
                 respawn_delay=2.0,
                 parameters=[configured_params],
                 arguments=["--ros-args", "--log-level", log_level],
+                remappings=[("cmd_vel", "cmd_vel_nav")],
             ),
             Node(
                 package="nav2_bt_navigator",
@@ -189,7 +190,7 @@ def generate_launch_description():
                 respawn_delay=2.0,
                 parameters=[configured_params],
                 arguments=["--ros-args", "--log-level", log_level],
-                remappings=[("cmd_vel", "cmd_vel_nav"), ("cmd_vel_smoothed", "cmd_vel")],
+                remappings=[("cmd_vel", "cmd_vel_nav"), ("cmd_vel_smoothed", "nav_cmd_vel_stamped")],
             ),
             Node(
                 package="nav2_lifecycle_manager",
@@ -234,6 +235,7 @@ def generate_launch_description():
                 plugin="behavior_server::BehaviorServer",
                 name="behavior_server",
                 parameters=[configured_params],
+                remappings=[("cmd_vel", "cmd_vel_nav")],
             ),
             ComposableNode(
                 package="nav2_bt_navigator",
@@ -252,7 +254,7 @@ def generate_launch_description():
                 plugin="nav2_velocity_smoother::VelocitySmoother",
                 name="velocity_smoother",
                 parameters=[configured_params],
-                remappings=[("cmd_vel", "cmd_vel_nav"), ("cmd_vel_smoothed", "cmd_vel")],
+                remappings=[("cmd_vel", "cmd_vel_nav"), ("cmd_vel_smoothed", "nav_cmd_vel_stamped")],
             ),
             ComposableNode(
                 package="nav2_lifecycle_manager",
